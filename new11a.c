df8048507d0bf2fdefaac4c651c4e9a70f3b4724
emka82mmm2 <43217102+emka82mmm2@users.noreply.github.com>
2018-09-13 15:21:44 +0200

    Update new11a.c
    Calculating of 3d matrix by MK

--- new11a.c
+++ new11a.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 
-int main 
-  {
+int main()
+{
+  int y = 110;
+  int z = 110;
+  int t = 110;
+
   
-    printf( "Hello There" );
+ 
+  int array[y][z][t]; /* Declares an array */
   
-  }
+ 	 
+    		for ( int y = 100; y > 1; y-- ) {
+        		for( int z = 100; z > 1; z-- ) {
+				        for( int t = 100; t > 1 ;  t-- ) {
+  	
+          					printf( "[%d][%d][%d]=%d\t"); printf(" y, z , t: \t", y,z,t ); printf("array[y][z][t]: \t" , array[y][z][t] ); printf("[%d][%d][%d]\n" ); 
+							
+				}
+					}
+						}
+
+
+						float q = 0.000001;
+						float a = 0.000002;
+						float v = 0.000003;
+
+	
+          
+          for ( q = 1.968795 ; q < 10; q++ ) {
+							for ( a = 2.345633; a < 20; a++ ) {
+								for ( v = 3.345251; v < 30; v++ ) {
+									printf("%d%d%di=%d\t] = %d\t"); printf( "q,a,v: \n", q,a,v );
+
+					
+  		}
+			  }
+				  }
+					
+						
+							
+
+					
+  
+		
+		
+
+}
+
