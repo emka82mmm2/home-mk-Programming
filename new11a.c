4abed44551917743d727fe01890115bb6475cbfe
emka82mmm2 <43217102+emka82mmm2@users.noreply.github.com>
2018-09-13 16:04:46 +0200

    Update new11a.c

--- new11a.c
+++ new11a.c
@@ -13,10 +13,14 @@ int main()
  	 
     		for ( int y = 100; y > 1; y-- ) {
         		for( int z = 100; z > 1; z-- ) {
-				        for( int t = 100; t > 1 ;  t-- ) {
+				for( int t = 100; t > 1 ;  t-- ) {
   	
-          					printf( "[%d][%d][%d]=%d\t"); printf(" y, z , t: \t", y,z,t ); printf("array[y][z][t]: \t" , array[y][z][t] ); printf("[%d][%d][%d]\n" ); 
-							
+  
+  
+    					
+        					printf( "[%d][%d][%d]=%d\t"); printf(" y, z , t: \t", y,z,t ); printf("array[y][z][t]: \t" , array[y][z][t] ); printf("[%d][%d][%d]\n" ); 
+				
+						
 				}
 					}
 						}
@@ -26,20 +30,37 @@ int main()
 						float a = 0.000002;
 						float v = 0.000003;
 
-	
-          
-          for ( q = 1.968795 ; q < 10; q++ ) {
+						for ( q = 1.968795 ; q < 10; q++ ) {
 							for ( a = 2.345633; a < 20; a++ ) {
 								for ( v = 3.345251; v < 30; v++ ) {
 									printf("%d%d%di=%d\t] = %d\t"); printf( "q,a,v: \n", q,a,v );
 
 					
   		}
-			  }
-				  }
+			}
+				}
 					
 						
-							
+			int s=1,d=5,f=7;
+			int g=4,h=5,j=6;
+			int l,p;
+
+		int qw[d][f];
+		int mkv[h][j];
+		int mkvv[l][p];
+				
+				printf( "qw :", qw[d][f] );
+			
+					mkvv[l][p] = mkv[h][j] + qw[d][f];
+
+					printf( "mkvv : " , mkvv[l][p] );
+						
+						printf( "mkv :\t", mkv[h][j] );
+
+							l = d+h;
+							p = f+j;
+
+							printf( "qw + mkv = \t" , mkvv[l][p] );
 
 					
   
@@ -47,4 +68,6 @@ int main()
 		
 
 }
+
+
 
